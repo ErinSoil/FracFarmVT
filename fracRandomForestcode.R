2f5b8e1c217da5c59600ffcab6bd069ebe865aaa
ErinSoil <124820333+ErinSoil@users.noreply.github.com>
2024-04-18 16:11:20 -0400

    started working on new code

--- fracRandomForestcode.R
+++ fracRandomForestcode.R
@@ -82,7 +82,6 @@ model_data_1 <- Predictor$new(RFtestP, data =  fracData_rmALLgP %>%
 pdp_all <- FeatureEffects$new(model_data_1, method = "pdp")
 plot(pdp_all)
 
-
 #create partial dependance plots for PropMAOM
 model_data_1 <- Predictor$new(RFtestprop, data = fracData_rmALL %>%
                                 dplyr::select(-propM))
@@ -104,24 +103,25 @@ model_data_SHprop <- Predictor$new(RFtestSHprop, data =  fracData_soilhealthprop
 pdp_all <- FeatureEffects$new(model_data_SHprop, method = "pdp")
 plot(pdp_all)
 
+#conduct Random Forest for total Carbon and total active carbon
 
-##Old Code and NOTES
+
+
+##Old Code and NOTES###############################################################
 # create a new df with select only continuous variables this results in no data. 
 fracDataNum <- fracData 
 select_if(is.numeric)
 view(fracDataNum)
-
 #remove all rows (farm fields) that have a missing variable #this results in no data
 fracDataNum_noNA <- fracData %>%
   select_if(is.numeric)  %>%
   drop_na()
 view(fracDataNum_noNA)
-
 #summary of dataframe
 skimr::skim_without_charts(fracDataNum_noNA)
 #remove the columns with limited data
 fracData_rm <- fracData %>%
-  select(#list all the columns I want to keep with commas, no ") %>%
+  #select(#list all the columns I want to keep with commas, no ") %>%
     #remove the rows with ~25ish missing data 
     drop_na()
     
@@ -130,7 +130,7 @@ fracData_rm <- fracData %>%
       drop_na() 
     
     view(fracData_rm)
-    #grouped by farm type? #ask Sophie if I've done this correctly, need to figure out what to replace rf.afsis with
+    #grouped by farm type? 
     #fracData <- rf.afsis %>% 
     ##select(-Type.x)
     
@@ -149,242 +149,11 @@ fracData_rm <- fracData %>%
     #   dplyr::filter(Type.x == "Corn") %>% 
     #   select(-Type.x)
     
-    head(fracData)
-    
+    #head(fracData)
     
     #conduct RF for different variables as the predicted mgCpergSoilM, mgCpergSoilP, propM, total carbon, aggregate stability
     #add different variables, like management data to each analysis (when analyzing the mang data, make the NAs "other")
     # do the same as above for the lmm as well as the rf
     
     
-
-********************************************************************
-
-#setwd("/Users/f003833/Documents/GitHub/FracFarmVT") #caitlin
-setwd("C:/Users/F004SPC/Documents/GitHub/FracFarmVT") #erin
-
-##call in the analytical data
-fracData <- read.csv(file="fracData.csv", header=TRUE, sep=",")
-
-str(fracData)
-
-#Create a new dataframe with more variables for the rf propM
-fracData_rmALLpm <- fracData
-  select(active_carbon,ph,ppt.cm,tmeanC,overall.score, soil_texture_clay, organic_matter, aggregate_stability, pred_water_capacity, propM) 
-  #remove the rows with ~25ish missing data 
-  drop_na() 
-
-# create a new dataframe with more variables for the rf mgCpergSoilM
-fracData_rmALLgM<- fracData %>%
-  select(active_carbon,ph,ppt.cm,tmeanC,overall.score, soil_texture_clay, organic_matter, aggregate_stability, pred_water_capacity, mgCpergSoilM) %>%
-  #remove the rows with ~25ish missing data 
-  drop_na() 
-
-# create a new dataframe with more variables for the rf mgCpergSoilP
-fracData_rmALLgP<- fracData %>%
-  select(active_carbon,ph,ppt.cm,tmeanC,overall.score, soil_texture_clay, organic_matter, aggregate_stability, pred_water_capacity, mgCpergSoilP) %>%
-  #remove the rows with ~25ish missing data 
-  drop_na() 
-
-# create a new dataframe with only a couple variables (mgCpergSoilM)
-fracData_soilhealthM <- fracData %>%
-  select(overall.score, mgCpergSoilM) %>%
-  #remove the rows with ~25ish missing data 
-  drop_na() 
-
-view(fracData_soilhealthM)
-# create a new dataframe with only a couple variables (mgCpergSoilP)
-fracData_soilhealthP <- fracData %>%
-  select(overall.score, mgCpergSoilP) %>%
-  #remove the rows with ~25ish missing data 
-  drop_na() 
-
-view(fracData_soilhealthP)
-
-fracData_soilhealthpropM <- fracData %>%
-  select(overall.score, propM) %>%
-  #remove the rows with ~25ish missing data 
-  drop_na() 
-
-view(fracData_soilhealthpropM)
-
-
-
-
-
-
-
-
-
-+++++++++++++++++++++++
-# create a new df with select only continuous variables this results in no data. 
-fracDataNum <- fracData 
-  select_if(is.numeric)
-view(fracDataNum)
-
-#remove all rows (farm fields) that have a missing variable #this results in no data
-fracDataNum_noNA <- fracData %>%
-  select_if(is.numeric)  %>%
-  drop_na()
-view(fracDataNum_noNA)
-
-#summary of dataframe
-skimr::skim_without_charts(fracDataNum_noNA)
-
-
-#remove the columns with limited data
-fracData_rm <- fracData %>%
-  select(#list all the columns I want to keep with commas, no ") %>%
- #remove the rows with ~25ish missing data 
- drop_na()
-
- fracData_rm <- fracData %>%
-   select(active_carbon,ph,ppt.cm, mgCpergSoilP, Type.x) %>% 
-   #remove the rows with ~25ish missing data 
-    drop_na() 
- 
- view(fracData_rm)
- 
- 
- # create a new dataframe with more variables for the rf propM
- fracData_rmALLpm <- fracData %>%
-   select(active_carbon,ph,ppt.cm,tmeanC,overall.score, soil_texture_clay, organic_matter, aggregate_stability, pred_water_capacity, propM) %>%
-   #remove the rows with ~25ish missing data 
-   drop_na() 
- 
- # create a new dataframe with more variables for the rf mgCpergSoilM
- fracData_rmALLgM<- fracData %>%
-   select(active_carbon,ph,ppt.cm,tmeanC,overall.score, soil_texture_clay, organic_matter, aggregate_stability, pred_water_capacity, mgCpergSoilM) %>%
-   #remove the rows with ~25ish missing data 
-   drop_na() 
- 
-   # create a new dataframe with more variables for the rf mgCpergSoilP
-   fracData_rmALLgP<- fracData %>%
-   select(active_carbon,ph,ppt.cm,tmeanC,overall.score, soil_texture_clay, organic_matter, aggregate_stability, pred_water_capacity, mgCpergSoilP) %>%
-     #remove the rows with ~25ish missing data 
-     drop_na() 
-   
-   #remove the rows with ~25ish missing data 
-   drop_na()  
-
- # create a new dataframe with only a couple variables
- fracData_soilhealthM <- fracData %>%
-   select(overall.score, mgCpergSoilM) %>%
-   #remove the rows with ~25ish missing data 
-   drop_na() 
- 
- view(fracData_soilhealthM)
- 
- fracData_soilhealthP <- fracDataNum %>%
-   select(overall.score, mgCpergSoilP) %>%
-   #remove the rows with ~25ish missing data 
-   drop_na() 
- 
- view(fracData_soilhealthP)
- 
- fracData_soilhealthpropM <- fracDataNum %>%
-   select(overall.score, propM) %>%
-   #remove the rows with ~25ish missing data 
-   drop_na() 
- 
- view(fracData_soilhealthpropM)
-
- #load library
-library(ranger)
-library(skimr)
-library(iml)
-library(tidyverse)
-
-#Random forest: generalizable models since it is an ensemble of multiple decorrelated trees.
-#select variables of interest by using different dataframes created above
-
-
-RFtest <- ranger(mgCpergSoilM ~ ., data = fracData_soilhealthM, importance = "permutation")
-RFtest$variable.importance
-
-RFtestP <- ranger(mgCpergSoilP ~ ., data = fracData_soilhealthP, importance = "permutation")
-RFtestP$variable.importance
-
-RFtestprop <- ranger(propM ~ ., data = fracData_soilhealthpropM, importance = "permutation")
-RFtestprop$variable.importance
-#negative relationship with Prop M and soil health
-
-
-
-#run with more variables #change the dataset fracData_rmALL to include dependant variable each time
-RFtest1 <- ranger(mgCpergSoilM ~ ., data = fracData_rmALLgM, importance = "permutation")
-RFtest1$variable.importance
-
-RFtest2 <- ranger(mgCpergSoilP ~ ., data = fracData_rmALLgP, importance = "permutation")
-RFtest2$variable.importance
-
-RFtest3 <- ranger(propM ~ ., data = fracData_rmALLpm, importance = "permutation")
-RFtest3$variable.importance
-
-RFtestpropM2 <- ranger(propM ~ ., data = fracData_rmALL, importance = "permutation")
-RFtestpropM2$variable.importance
-
-
-#run for soilHealth
-RFtest2 <- ranger(mgCpergSoilM ~ ., data = fracData_soilhealth, importance = "permutation")
-# this runs the stats on the random forest
-RFtest2
-
-
-#show each variable's relative importance
-RFtest2$variable.importance
-
-
-#create partial dependance plots for mgCpergSoil(MAOM)
-
-model_data_1 <- Predictor$new(RFtest1, data =  fracData_rmALLgM %>%
-                                dplyr::select(-mgCpergSoilM))
-
-pdp_all <- FeatureEffects$new(model_data_1, method = "pdp")
-plot(pdp_all)
-
-
-#create partial dependance plots for mgCpergSoil(POM)
-
-model_data_1 <- Predictor$new(RFtest1, data =  fracData_rmALLgP %>%
-                                dplyr::select(-mgCpergSoilP))
-
-pdp_all <- FeatureEffects$new(model_data_1, method = "pdp")
-plot(pdp_all)
-
-
-#create partial dependance plots for PropMAOM
-
-model_data_1 <- Predictor$new(RFtest1, data = fracData_rmALL %>%
-                                dplyr::select(-propM))
-
-pdp_all <- FeatureEffects$new(model_data_1, method = "pdp")
-plot(pdp_all)
-
-
-#grouped by farm type? #ask Sophie if I've done this correctly, need to figure out what to replace rf.afsis with
-#fracData <- rf.afsis %>% 
- ##select(-Type.x)
-
-#fracData <- rf.afsis %>% 
- # dplyr::filter(Type.x == "Pasture") %>% 
-  #select(-Type.x)
-#fracData <- rf.afsis %>% 
- # dplyr::filter(Type.x == "Hay") %>% 
-  #select(-Type.x)
-
-#fracData <- rf.afsis %>% 
- # dplyr::filter(Type.x == "Field crops") %>% 
-  #select(-Type.x)
-
-# fracData <- rf.afsis %>% 
-#   dplyr::filter(Type.x == "Corn") %>% 
-#   select(-Type.x)
-
-head(fracData)
-
-
-#conduct RF for different variables as the predicted mgCpergSoilM, mgCpergSoilP, propM, total carbon, aggregate stablity
-#add different variables, like management data to each analysis (when analysing the mang data, make the NAs "other")
-# do the same as above for the lmm as well as the rf
 
